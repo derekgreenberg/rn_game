e20129552028f32fe72132b9383d7f466c16aba3
derekgreenberg <derekg@gmail.com>
2016-02-23 11:56:11 -0800

    Fixed mounted component test

--- test/components/GameBoardSpec.js
+++ test/components/GameBoardSpec.js
@@ -3,15 +3,18 @@ import { shallow, mount, describeWithDOM } from 'enzyme'
 import { expect } from 'chai'
 import GameBoard from '../../App/Components/GameBoard'
 import GameTile from '../../App/Components/GameTile'
+import configureStore from '../../App/Store/ConfigureStore'
+import { Provider } from 'react-redux'
 
 describe('<GameBoard />', () => {
-  // describeWithDOM('Tiles', () => {
-  //   it('renders a 9 x 15 grid of GameTiles', () => {
-  //     const wrapper = mount(<GameBoard />)
-  //     expect(wrapper.length).to.equal(1)
-  //     const tiles = wrapper.find(GameTile)
-  //     expect(tiles.length).to.equal(135)
-  //   })
-  // })
+  describeWithDOM('Tiles', () => {
+    it('renders a 9 x 15 grid of GameTiles', () => {
+      const store = configureStore()
+      const wrapper = mount(<Provider store={store}><GameBoard /></Provider>)
+      expect(wrapper.length).to.equal(1)
+      const tiles = wrapper.find(GameTile)
+      expect(tiles.length).to.equal(135)
+    })
+  })
 })
 
